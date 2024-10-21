f9a7ad2fbf12e938b820d389399468fcdec4a18b
Hyelnati Deborah <117924540+Hyelnati@users.noreply.github.com>
2024-10-21 01:57:08 +0100

    Revert "new commit"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      fontFamily: {
-        customBold: ["FontBold", "sans-serif"],
-        customLight: ["FontLight", "sans-serif"],
-        customMedium: ["FontMedium", "sans-serif"],
-        customRegular: ["FontRegular", "sans-serif"],
-        customBlack: ["FontBlack", "sans-serif"],
-        ExtraBold: ["ExtraBold", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
